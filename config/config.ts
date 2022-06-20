0edb2a5cf931ca9437d4f6fa1e5b319e68c3622b
wangzhongyong <wzy@kgtdata.com>
2022-06-20 15:37:10 +0800

    fix: 配置externals

--- config/config.ts
+++ config/config.ts
@@ -4,10 +4,26 @@ import routes from './routes';
 export default defineConfig({
   hash: true,
   ssr: {},
+  devtool: 'eval',
   exportStatic: {},
   nodeModulesTransform: {
     type: 'none',
   },
+
+  externals: {
+    moment: 'window.moment',
+    react: 'window.React',
+    'react-dom': 'window.ReactDOM',
+    echarts: 'window.echarts',
+    antd: 'window.antd',
+  },
+  scripts: [
+    'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.3/moment.min.js',
+    'https://gw.alipayobjects.com/os/lib/react/16.13.1/umd/react.production.min.js',
+    'https://gw.alipayobjects.com/os/lib/react-dom/16.13.1/umd/react-dom.production.min.js',
+    'https://cdn.jsdelivr.net/npm/echarts@5.3.3/dist/echarts.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/antd/4.21.3/antd.min.js',
+  ],
   dynamicImport: {
     loading: '@/pages/components/Loading',
   },
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import styles from './index.less';
 import { Button, Input } from 'antd';
+import * as echarts from 'echarts';
 import {
   useState,
   useEffect,
@@ -106,6 +107,27 @@ export default function IndexPage() {
   const handleClick = (num: number) => {};
   useEffect(() => {
     console.log('服务端ssr');
+
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById('myChart'));
+    // 绘制图表
+    myChart.setOption({
+      title: {
+        text: 'ECharts 入门示例',
+      },
+      tooltip: {},
+      xAxis: {
+        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子'],
+      },
+      yAxis: {},
+      series: [
+        {
+          name: '销量',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20],
+        },
+      ],
+    });
   }, []);
   const columns = [
     {
@@ -255,6 +277,7 @@ export default function IndexPage() {
         search={true}
         customFilter={renderFilter()}
       />
+      <div id="myChart" style={{ width: 500, height: 500 }}></div>
     </div>
   );
 }
