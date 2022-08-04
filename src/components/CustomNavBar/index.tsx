691178db2c25e60057c1cf762d1a5eb1edf8c207
wangzhongyong <wzy@kgtdata.com>
2022-08-04 17:29:42 +0800

    嵌套遍历路由

--- src/components/CustomNavBar/index.tsx
+++ src/components/CustomNavBar/index.tsx
@@ -1,7 +1,9 @@
 import React, { FC, useEffect, useState } from 'react';
 import { NavBar } from 'antd-mobile';
 import { LeftOutline } from 'antd-mobile-icons';
-import { useHistory, useLocation, useModel } from 'umi';
+import { useHistory, useLocation } from 'umi';
+import { getCurrentRoute } from '@/utils/util';
+import routes from '../../../config/routes';
 interface NavBarProps {
   backArrow?: boolean;
   showTitle?: boolean;
@@ -15,7 +17,13 @@ const CustomNavBar: FC<NavBarProps> = ({
   const history = useHistory();
   const location = useLocation();
   const [title, setTitle] = useState('');
-  useEffect(() => {}, []);
+  useEffect(() => {
+    console.log('routes', routes);
+
+    const currentRoute = getCurrentRoute(routes, location.pathname);
+    setTitle(currentRoute?.title);
+    console.log('title', title);
+  }, [location.pathname]);
   const back = () => {
     history.goBack();
   };
