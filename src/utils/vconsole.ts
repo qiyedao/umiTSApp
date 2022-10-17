778102946e6df1abf3418b48578972eac422f2b2
wangzhongyong <wzy@kgtdata.com>
2022-10-17 17:02:04 +0800

    fix: add another selector

--- src/utils/vconsole.ts
+++ src/utils/vconsole.ts
@@ -2,6 +2,7 @@ export const vConsole = () => {
   if (window.location.href.includes('debug=1')) {
     let scriptToAdd = document.createElement('script');
     scriptToAdd.src = '/vconsole.js';
-    document.head.appendChild(scriptToAdd);
+    // document.head.appendChild(scriptToAdd);
+    document.querySelector('head')?.appendChild(scriptToAdd);
   }
 };
