6006d3dedd4bd55af57f3504d991c61524b23393
qiye <1710797241@qq.com>
2022-11-17 15:12:36 +0800

    fix: rm class

--- src/pages/403.tsx
+++ src/pages/403.tsx
@@ -2,15 +2,12 @@ import { Button, Result } from 'antd-mobile';
 import React from 'react';
 import { history } from 'umi';
 const NoFoundPage: React.FC = () => (
-  <div className=" h-full w-full bg-stone-400">
+  <div className=" h-full w-full">
     <Result
       status="error"
       title="无权访问"
       description={
-        <Button
-          color="primary"
-          onClick={() => history.push('/' + window.location.search)}
-        >
+        <Button color="primary" onClick={() => history.push('/' + window.location.search)}>
           回主页
         </Button>
       }
