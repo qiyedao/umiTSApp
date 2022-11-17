6fc66953040f3d58bfa631f57378f128870092e0
qiye <1710797241@qq.com>
2022-11-17 17:01:39 +0800

    fix: add post mock

--- mock/index.ts
+++ mock/index.ts
@@ -10,18 +10,28 @@ type IRestult = {
 };
 export default {
   'GET /user': (req: any, res: { send: (arg0: IRestult) => void }) => {
-    console.log('req', req.params);
+    console.log('req', req.query, 'query', req.params);
 
     res.send({
       code: 1,
       data: {
         name: 'yiyi',
+        query: req.query,
+      },
+    });
+  },
+  'POST /user': (req: any, res: { send: (arg0: IRestult) => void }) => {
+    console.log('req', req.body, 'body');
+
+    res.send({
+      code: 1,
+      data: {
+        name: 'yiyi',
+        body: req.body,
       },
     });
   },
   'GET /v1/user': mockjs.mock({
-    'list|100': [
-      { 'rowKey|1-100': 50, name: '@city', 'value|1-100': 50, 'type|0-2': 1 },
-    ],
+    'list|100': [{ 'rowKey|1-100': 50, name: '@city', 'value|1-100': 50, 'type|0-2': 1 }],
   }),
 };
